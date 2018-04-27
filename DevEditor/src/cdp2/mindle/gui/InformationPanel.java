8861c75f38380f368d8a7ad44bb5a10fb911bc6d
Etheon <snow_chris@naver.com>
2018-04-27 17:47:37 +0900

    window builder를 활용해서 정보 패널 UI 개발

--- DevEditor/src/cdp2/mindle/gui/InformationPanel.java
+++ DevEditor/src/cdp2/mindle/gui/InformationPanel.java
@@ -1,9 +1,13 @@
 package cdp2.mindle.gui;
 
-import java.awt.Dimension;
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -12,163 +16,173 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.UIManager;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
 
-import cdp2.mindle.manager.InformationManager;
+public class InformationPanel extends JPanel {
+	private JTextField nameInputField;
+	private JTextField codeInputField;
+	private JTable extensionTable;
 
-public class InformationPanel extends JPanel
-{
-	InformationManager informationManager;
-	JLabel[] labels;
-	JTextField nameInputField;
-	JTextField codeInputField;
-	JCheckBox targetMaleCheckbox;
-	JCheckBox targetFemaleCheckbox;
-	JCheckBox targetOldCheckbox;
-	JCheckBox targetAdultCheckbox;
-	JCheckBox targetTeenCheckbox;
-	JCheckBox targetChildCheckbox;
-	JComboBox<String> languageCombobox;
-	JButton addButton;
-	JButton deleteButton;
-	JTable extensionInfoTable;
-	JScrollPane extensionPane;
-	
-	final String[] labelString = new String[] {
-			"¿Ã∏ß : ", 
-			"Ωƒ∫∞ ƒ⁄µÂ : ",
-			"¥ÎªÛ",
-			"(¡ﬂ∫π ∞°¥…)",
-			"ææÓ : ",
-			"√ﬂ∞° ¡§∫∏"
-	};
-	final String[] nationString = new String[] {
-			"Korean", "English", "French", "Japanese", "Chinese"
-	};
-	
-	public InformationPanel()
-	{
-		informationManager = new InformationManager();
-		labels = new JLabel[6];
-		for (int i = 0; i < labels.length; ++i) {
-			labels[i] = new JLabel(labelString[i]);
-			labels[i].setFont(new Font("±º∏≤", Font.PLAIN, 15));
-		}
-		nameInputField = new JTextField(40);
-		codeInputField = new JTextField(40);
-		languageCombobox = new JComboBox<>(nationString);
-		addButton = new JButton("√ﬂ∞°");
-		deleteButton = new JButton("ªË¡¶");
-		
-		initExtensionInfoTable();
-		initTargetCheckbox();
-		initLayout();
-	}
-	
-	private void initTargetCheckbox()
-	{
-		targetMaleCheckbox = new JCheckBox("≥≤º∫");
-		targetFemaleCheckbox = new JCheckBox("ø©º∫");
-		targetOldCheckbox = new JCheckBox("≥Î¿Œ");
-		targetAdultCheckbox = new JCheckBox("º∫¿Œ");
-		targetTeenCheckbox = new JCheckBox("√ªº“≥‚");
-		targetChildCheckbox = new JCheckBox("æ∆µø");
-		
-		targetMaleCheckbox.setFont(new Font("±º∏≤", Font.PLAIN, 15));
-		targetFemaleCheckbox.setFont(new Font("±º∏≤", Font.PLAIN, 15));
-		targetOldCheckbox.setFont(new Font("±º∏≤", Font.PLAIN, 15));
-		targetAdultCheckbox.setFont(new Font("±º∏≤", Font.PLAIN, 15));
-		targetTeenCheckbox.setFont(new Font("±º∏≤", Font.PLAIN, 15));
-		targetChildCheckbox.setFont(new Font("±º∏≤", Font.PLAIN, 15));
-	}
-	
-	private void initExtensionInfoTable()
-	{
-		String columnNames[] =
-			{ "No.", "≈∏¿‘", "¡§∫∏", "º±≈√"};
+	/**
+	 * Create the panel.
+	 */
+	public InformationPanel() {
+		setBackground(new Color(250, 235, 215));
+		setForeground(UIManager.getColor("Button.light"));
 
-			Object rowData[][] =
-			{
-			{ 1, "¿˙¿⁄", "J.Smith", false },
-			{ 2, "¿€º∫≥‚µµ", "1996", false },
-			{ 3, "¿€º∫¿œ¿⁄", "2018.4.27", false }
-			};
-			
-			extensionInfoTable = new JTable(rowData, columnNames);
-			extensionInfoTable.getColumnModel().getColumn(0).setMaxWidth(50);
-			extensionInfoTable.getColumnModel().getColumn(3).setMaxWidth(50);
-			for (int i = 0; i < rowData.length; ++i) {
-				extensionInfoTable.setRowHeight(i, 20);
+		JLabel nameLabel = new JLabel("\uC774\uB984");
+		nameLabel.setFont(new Font("±º∏≤", Font.BOLD, 15));
+
+		JLabel codeLabel = new JLabel("\uC2DD\uBCC4 \uCF54\uB4DC");
+		codeLabel.setFont(new Font("±º∏≤", Font.BOLD, 15));
+
+		JLabel targetLabel = new JLabel("\uB300\uC0C1");
+		targetLabel.setFont(new Font("±º∏≤", Font.BOLD, 15));
+
+		JLabel duplicateLabel = new JLabel("(\uC911\uBCF5 \uAC00\uB2A5)");
+		duplicateLabel.setFont(new Font("±º∏≤", Font.BOLD, 15));
+
+		JLabel languageLabel = new JLabel("\uC5B8\uC5B4");
+		languageLabel.setFont(new Font("±º∏≤", Font.BOLD, 15));
+
+		JLabel extensionLabel = new JLabel("\uCD94\uAC00\uC815\uBCF4");
+		extensionLabel.setFont(new Font("±º∏≤", Font.BOLD, 15));
+
+		nameInputField = new JTextField();
+		nameInputField.setColumns(10);
+
+		codeInputField = new JTextField();
+		codeInputField.setColumns(10);
+
+		JCheckBox maleCheckbox = new JCheckBox("\uB0A8\uC131");
+		maleCheckbox.setBackground(new Color(250, 235, 215));
+		maleCheckbox.setFont(new Font("±º∏≤", Font.PLAIN, 15));
+
+		JCheckBox femaleCheckbox = new JCheckBox("\uC5EC\uC131");
+		femaleCheckbox.setBackground(new Color(250, 235, 215));
+		femaleCheckbox.setFont(new Font("±º∏≤", Font.PLAIN, 15));
+
+		JCheckBox oldCheckbox = new JCheckBox("\uB178\uC778");
+		oldCheckbox.setBackground(new Color(250, 235, 215));
+		oldCheckbox.setFont(new Font("±º∏≤", Font.PLAIN, 15));
+
+		JCheckBox adultCheckbox = new JCheckBox("\uC131\uC778");
+		adultCheckbox.setBackground(new Color(250, 235, 215));
+		adultCheckbox.setFont(new Font("±º∏≤", Font.PLAIN, 15));
+
+		JCheckBox teenCheckbox = new JCheckBox("\uCCAD\uC18C\uB144");
+		teenCheckbox.setBackground(new Color(250, 235, 215));
+		teenCheckbox.setFont(new Font("±º∏≤", Font.PLAIN, 15));
+
+		JCheckBox childCheckbox = new JCheckBox("\uC544\uB3D9");
+		childCheckbox.setBackground(new Color(250, 235, 215));
+		childCheckbox.setFont(new Font("±º∏≤", Font.PLAIN, 15));
+
+		JComboBox languageSelectCombobox = new JComboBox();
+		languageSelectCombobox.setModel(new DefaultComboBoxModel(new String[] { "Korean", "Japanese", "Chinese" }));
+
+		JScrollPane scrollPane = new JScrollPane();
+
+		JButton addButton = new JButton("+");
+		addButton.setBackground(Color.WHITE);
+		addButton.setFont(new Font("±º∏≤", Font.PLAIN, 10));
+
+		JButton deleteButton = new JButton("-");
+		deleteButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 			}
-			extensionInfoTable.setFont(new Font("±º∏≤", Font.PLAIN, 20));
-			extensionPane = new JScrollPane(extensionInfoTable);
-			extensionPane.setPreferredSize(new Dimension(300, 10));
-	}
-	
-	private void initLayout()
-	{
-		GroupLayout layout = new GroupLayout(this);
-		setLayout(layout);
-		layout.setVerticalGroup(
-				   layout.createSequentialGroup()
-				      .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				           .addComponent(labels[0])
-				           .addComponent(nameInputField))
-				      .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-					           .addComponent(labels[1])
-					           .addComponent(codeInputField))
-				      .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-					           .addComponent(labels[2])
-					           .addComponent(targetMaleCheckbox)
-					           .addComponent(targetFemaleCheckbox))
-				      .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-					           .addComponent(labels[3])
-					           .addComponent(targetOldCheckbox)
-					           .addComponent(targetAdultCheckbox)
-					           .addComponent(targetTeenCheckbox)
-					           .addComponent(targetChildCheckbox))
-				      .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-					           .addComponent(labels[4])
-					           .addComponent(languageCombobox))
-				      .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-					           .addComponent(labels[5])
-					           .addComponent(addButton)
-					           .addComponent(deleteButton))
-				      .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
-				    		  .addComponent(extensionPane))
-				);
-		
-		layout.setHorizontalGroup(
-				   layout.createSequentialGroup()
-				      .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				           .addComponent(labels[0])
-				           .addComponent(labels[1])
-				           .addComponent(labels[2])
-				           .addComponent(labels[3])
-				           .addComponent(labels[4])
-				           .addComponent(labels[5])
-				           )
-				      .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				    		  .addComponent(nameInputField)
-				    		  .addComponent(codeInputField)
-				    		  .addGroup(layout.createSequentialGroup()
-				    				  .addComponent(targetMaleCheckbox)
-				    				  .addComponent(targetFemaleCheckbox)
-				    				  )
-				    		  .addGroup(layout.createSequentialGroup()
-				    				  .addComponent(targetOldCheckbox)
-				    				  .addComponent(targetAdultCheckbox)
-				    				  .addComponent(targetTeenCheckbox)
-				    				  .addComponent(targetChildCheckbox)
-				    				  )
-				    		  .addGroup(layout.createSequentialGroup()
-				    				  .addComponent(languageCombobox)
-				    		  )
-				    		  .addGroup(layout.createSequentialGroup()
-				    				  .addComponent(addButton)
-				    				  .addComponent(deleteButton)
-				    		  )
-				    		  .addComponent(extensionPane)
-				    	)
-				);
+		});
+		deleteButton.setFont(new Font("±º∏≤", Font.PLAIN, 10));
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+						.addGroup(
+								groupLayout.createParallelGroup(Alignment.LEADING)
+										.addGroup(groupLayout.createSequentialGroup().addContainerGap()
+												.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+														.addComponent(duplicateLabel).addComponent(extensionLabel)
+														.addComponent(codeLabel)))
+										.addGroup(groupLayout.createSequentialGroup().addContainerGap()
+												.addComponent(nameLabel))
+										.addGroup(groupLayout
+												.createSequentialGroup().addContainerGap().addComponent(targetLabel))
+										.addGroup(groupLayout
+												.createSequentialGroup().addContainerGap().addComponent(languageLabel)))
+						.addGap(26)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
+								.createSequentialGroup()
+								.addComponent(addButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED).addComponent(deleteButton)
+								.addContainerGap()).addGroup(groupLayout.createSequentialGroup()
+										.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 307, Short.MAX_VALUE)
+										.addContainerGap())
+								.addGroup(groupLayout
+										.createSequentialGroup()
+										.addGroup(groupLayout
+												.createParallelGroup(Alignment.LEADING)
+												.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+														.addComponent(codeInputField).addComponent(nameInputField,
+																GroupLayout.DEFAULT_SIZE, 307, Short.MAX_VALUE))
+												.addGroup(groupLayout.createSequentialGroup().addComponent(maleCheckbox)
+														.addGap(18).addComponent(femaleCheckbox))
+												.addGroup(groupLayout.createSequentialGroup().addComponent(oldCheckbox)
+														.addGap(18).addComponent(adultCheckbox).addGap(18)
+														.addComponent(teenCheckbox).addGap(18)
+														.addComponent(childCheckbox))
+												.addComponent(languageSelectCombobox, GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+										.addGap(19)))));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
+				.createSequentialGroup().addContainerGap()
+				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(nameInputField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addComponent(nameLabel))
+				.addPreferredGap(ComponentPlacement.UNRELATED)
+				.addGroup(groupLayout
+						.createParallelGroup(Alignment.BASELINE).addComponent(codeLabel).addComponent(codeInputField,
+								GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(ComponentPlacement.UNRELATED)
+				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(maleCheckbox)
+						.addComponent(femaleCheckbox).addComponent(targetLabel))
+				.addPreferredGap(ComponentPlacement.UNRELATED)
+				.addGroup(groupLayout
+						.createParallelGroup(Alignment.BASELINE).addComponent(duplicateLabel).addComponent(oldCheckbox)
+						.addComponent(adultCheckbox).addComponent(teenCheckbox).addComponent(childCheckbox))
+				.addPreferredGap(ComponentPlacement.UNRELATED)
+				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(languageSelectCombobox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addComponent(languageLabel))
+				.addPreferredGap(ComponentPlacement.UNRELATED)
+				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(extensionLabel)
+						.addComponent(addButton, GroupLayout.DEFAULT_SIZE, 28, Short.MAX_VALUE)
+						.addComponent(deleteButton, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(ComponentPlacement.UNRELATED)
+				.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE).addGap(33)));
+
+		extensionTable = new JTable();
+		extensionTable.setBackground(Color.WHITE);
+		extensionTable.setBorder(new LineBorder(new Color(0, 0, 0)));
+		extensionTable.setFont(new Font("±º∏≤", Font.PLAIN, 15));
+		extensionTable.setModel(new DefaultTableModel(
+				new Object[][] { { null, null, null, null }, { null, null, null, null }, { null, null, null, null }, },
+				new String[] { "No.", "\uD0C0\uC785", "\uC815\uBCF4", "\uC120\uD0DD" }) {
+			Class[] columnTypes = new Class[] { Integer.class, String.class, String.class, Boolean.class };
+
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		extensionTable.getColumnModel().getColumn(0).setPreferredWidth(39);
+		extensionTable.getColumnModel().getColumn(0).setMinWidth(10);
+		extensionTable.getColumnModel().getColumn(0).setMaxWidth(39);
+		extensionTable.getColumnModel().getColumn(3).setMaxWidth(42);
+		scrollPane.setViewportView(extensionTable);
+		setLayout(groupLayout);
+
 	}
 }
